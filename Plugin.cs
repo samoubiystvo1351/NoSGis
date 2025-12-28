7a7096b344ec2b93e9ead1d363e3d9be2c351f45
Computery <77170025+C0mputery@users.noreply.github.com>
2025-12-28 13:55:31 -0500

    General Improvements
    Added BepInEx.AssemblyPublicizer
    Fixed Color Checks
    Improved Code Cleanness
    Improved Preformance
    Added Documentation

--- Plugin.cs
+++ Plugin.cs
@@ -5,24 +5,32 @@ namespace BepInEx5.PluginTemplate
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private void Update()
-        {
-            if (PhotonNetwork.isMasterClient)
-            {
-                foreach (PhotonPlayer player in PhotonNetwork.otherPlayers)
-                {
-                    if (player.NickName == "Player_SGi")
-                    {
-                        foreach (PhotonView view in FindObjectsOfType<PhotonView>())
-                        {
-                            if (view.owner == player && view.gameObject.tag == "Player" && view.gameObject.name.StartsWith("TABS") && MainMenuHandler.IS_RED)
-                            {
-                                PhotonNetwork.CloseConnection(player);
-                                return;
-                            }
-                        }
-                    }
-                }
+        private void Update() { 
+            // Only run  on the master client
+            // If you are not the master client you cannot use the PhotonNetwork.CloseConnection call.
+            if (!PhotonNetwork.isMasterClient) { return; }
+
+            // We look for all network players in the scene
+            // This is not the best way of doing it, ideally you would patch the NetworkPlayer or something with Harmony.
+            // Then do the work there to avoid searching every frame, but the performance impact is negligible for this use case.
+            NetworkPlayer[] networkPlayers = FindObjectsOfType<NetworkPlayer>();
+            foreach (NetworkPlayer player in networkPlayers) {
+                // Ignore the local player to prevent weirdness
+                // mPhotonView is a private field, but made accessible via the BepInEx Assembly Publicizer
+                // This is the same photon view that the PlayerHandler for this player uses.
+                if (player.mPhotonView.isMine) { continue; }
+
+                // The IS_RED value gets passed on instantiation to the player object, this is how you retrieve it.
+                bool isRed = (bool)player.mPhotonView.instantiationData[0];
+                if (!isRed) { continue; } // the problematic players will never be on the blue team bcs of how there clients are edited.
+                
+                // Cache the owner for easier access
+                // PhotonView.owner is a property that does PhotonPlayer.Find(this.ownerId) internally.
+                // So caching it is more efficient.
+                PhotonPlayer owner = player.mPhotonView.owner;
+                
+                // Final check for the specific nickname, disconnect them if all conditions are met.
+                if (owner.NickName == "Player_SGi") { PhotonNetwork.CloseConnection(owner); }
             }
         }
     }
